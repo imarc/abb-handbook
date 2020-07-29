6752be331898ec0002fd91b53b8f0395351554de
Marcel Moreau <marcel@imarc.com>
2020-07-29 09:54:01 -0400

    master: initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // JS Goes here - ES6 supported
 
-import "./css/main.css";
+// import "./css/main.css";
+import "./scss/main.scss";
+import "./scss/_handbook/_handbook.scss";
+
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
